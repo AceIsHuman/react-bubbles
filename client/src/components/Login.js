a49a1559ad6db2507d06422f663f55f4cb11b242
AceIsHuman <menoman1995@yahoo.com>
2019-10-12 11:54:34 -0500

    set form value to creds state

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Icon, Header } from "semantic-ui-react";
 
 const Login = () => {
+  const [creds, setCreds] = useState({
+    username: null,
+    password: null
+  });
+
+  const handleChanges = e => {
+    setCreds({
+      ...creds,
+      [e.target.name]: e.target.value
+    });
+  };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
@@ -12,8 +23,18 @@ const Login = () => {
           <Icon name="user" />
           User Login
         </Header>
-        <Form.Input name="username" placeholder="Username" />
-        <Form.Input name="password" placeholder="Password" />
+        <Form.Input
+          name="username"
+          placeholder="Username"
+          value={creds.username}
+          onChange={handleChanges}
+        />
+        <Form.Input
+          name="password"
+          placeholder="Password"
+          value={creds.password}
+          onChange={handleChanges}
+        />
         <Button>Submit</Button>
       </Form>
     </>
